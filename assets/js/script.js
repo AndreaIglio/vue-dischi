ff7902ee9c9c93ec11de1377d2332f660b78b429
Andrea Iglio <andrea.iglio@gmail.com>
2021-01-11 14:55:12 +0100

    Completed Project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,33 @@
-let app = new Vue({
-    el:"#root",
-    data:{
+let app = new Vue ({
+
+
+    el: "#root",
+    
+    data: {
+
+        cdList : [],
 
     },
-    methods: {},
-    mounted() {
+    methods: {
         
     },
+    mounted() {
+        axios.get("https://flynn.boolean.careers/exercises/api/array/music").then(response => {
+            // console.log(response.data.response);
+            this.cdList = response.data.response;
+            console.log(this.cdList);
+        }).catch(error=>{
+            console.log(error);
+        })
+    },
+
 })
+
+
+
+// TODO:
+// Attraverso una chiamata ajax all’Api di boolean avremo a disposizione una decina di dischi musicali. 
+// Servendoci di Vue JS stampiamo tutto a schermo.
+
+// Bonus: Creare una select con i seguenti generi: pop, rock, metal e jazz. In base a cosa scegliamo nella select vedremo i corrispondenti cd.
+// Chiamata: https://flynn.boolean.careers/exercises/api/array/music
